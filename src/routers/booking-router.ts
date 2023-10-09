4b99e5b15c1969958bf8f3454c2f50bb68ed0664
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2023-10-09 09:54:55 -0300

    fix router

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { authenticateToken } from '@/middlewares';
+import { createUserBooking, getUserBooking, updateUserBooking } from '@/controllers/booking-controller';
 
 const bookingRouter = Router();
+
+bookingRouter
+  .all('/*', authenticateToken)
+  .get('/', getUserBooking)
+  .post('/', createUserBooking)
+  .put('/:bookingId', updateUserBooking);
 
 export { bookingRouter };
